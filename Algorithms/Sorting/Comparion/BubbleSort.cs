146404b2be8f98e78252c0d5d5410f46d6f75bcf
yattahajjaj <y.attahajjaj@gmail.com>
2019-12-16 18:35:04 -0600

    Update BubbleSort.cs

--- Algorithms/Sorting/Comparion/BubbleSort.cs
+++ Algorithms/Sorting/Comparion/BubbleSort.cs
@@ -1,5 +1,35 @@
-// Bubble Sort
-public class BubbleSort
+namespace Kunoichi.Algorithms.Sorting.ComparisonBased
 {
+// Bubble Sort
+  public class BubbleSort
+  {
+      public static void BubbleSort(int[] inputs)
+      {
+        if (inputs == null) return;
 
+        bool sorted;
+        int size = inputs.Length;
+
+       do
+       {
+         sorted = true;
+         for(int i= 1; i < size; i++)
+         {
+           if(inputs[i-1] > inputs[i])
+           {
+             Swap(inputs, i-1, i);
+             sorted = false;
+           }
+         }
+       }
+        while(!sorted)
+       }
+
+      public void Swap(int[] inputs, int i, int j)
+      {
+          int temp = inputs[i];
+          inputs[i] = inputs[j];
+          inputs[j] = temp;
+      }
+    }
 }
